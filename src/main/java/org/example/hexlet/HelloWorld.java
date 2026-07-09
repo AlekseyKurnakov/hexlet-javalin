9346ac9dafd6287c76cfb1dc394cbc5f73731d06
AlekseyKurnakov <kurnakov.a.v@mail.ru>
2026-07-09 06:18:31 +0300

    javaLin

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,17 +1,19 @@
 package org.example.hexlet;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import io.javalin.Javalin;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Создаем приложение
+        Javalin app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+        });
+
+        // Описываем, что загрузится по адресу /
+        app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/users", ctx -> ctx.result("GET /users"));
+
+        // Стартуем веб-сервер
+        app.start(7070);
     }
 }
